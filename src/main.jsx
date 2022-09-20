6eadfe936fb3ac634507c1f09047f7a07e680821
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-20 18:31:38 -0500

    feat(root): Add initial structure

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <h1>Hello World</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
