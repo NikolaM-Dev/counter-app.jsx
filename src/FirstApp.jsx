451f3a307976a949f973a26226af4f41f1da53a8
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-21 15:29:34 -0500

    fix(src): Fix typo error

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({ title, subtitle }) => {
   return (
     <>
       <h1>{title}</h1>
-      <h2>{subTitle}</h2>
+      <h2>{subtitle}</h2>
       <p>I&apos;m a subtitle</p>
     </>
   );
 };
 
 FirstApp.propTypes = {
-  subTitle: PropTypes.number,
+  subtitle: PropTypes.number,
   title: PropTypes.string.isRequired,
 };
 
 FirstApp.defaultProps = {
-  subTitle: 'Dragon Ball Z',
+  subtitle: 123,
   title: "Hello, I'm Nikola",
 };
